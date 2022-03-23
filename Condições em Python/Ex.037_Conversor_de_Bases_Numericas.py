508d7633ba64a826c094a22b09ae330a5f953e62
Lício Lima <liciomlima@gmail.com>
2022-03-23 09:44:11 -0300

    atualização

--- Condições em Python/Ex.037_Conversor_de_Bases_Numericas.py
+++ Condições em Python/Ex.037_Conversor_de_Bases_Numericas.py
@@ -6,6 +6,10 @@ print('''Escolha uma das bases para conversão:
 
 opcao = int(input('Sua opção: '))
 if opcao == 1:
-    print('{} convertido para BINÁRIO é igual {}'.format(num, bin(num)))
+    print('{} convertido para BINÁRIO é igual {}'.format(num, bin(num)[2:]))
 elif opcao == 2:
-    print('{} convertido para OCTAL')
+    print('{} convertido para OCTAL'.format(num, oct(num)[2:]))
+elif opcao == 3:
+    print('{} convertido para HEXADECIMAL é igual a {}'.format(num, hex(num)[2:]))
+else:
+    print('Opção inválida, tente novamente!')
